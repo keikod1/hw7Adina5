90c2016690bd08c1bd85931df8240f40a830615b
keikod1 <madragimova@icloud.com>
2022-01-28 13:12:37 +0600

    Initial Commit

---
+++ hw7Adina5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw7Adina5
+//
+//  Created by my macbook on 28/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
